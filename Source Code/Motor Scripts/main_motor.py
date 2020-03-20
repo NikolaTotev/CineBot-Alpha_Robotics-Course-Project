026fc56d1cc6c1fe9f0276324fcdce922fd5557d
Nikola <nikola.r.totev@gmail.com>
2020-03-20 01:43:34 -0900

    Updated motor script

---
+++ Source Code/Motor Scripts/main_motor.py
@@ -0,0 +1,21 @@
+import StepperController as controller
+import RPi.GPIO as GPIO
+from time import sleep
+
+def main():
+    MotorOne = controller.StepperManager(24,23,800)
+    MotorOne.MoveForward(0.0001, 90)
+    sleep(1)
+    MotorOne.MoveBackward(0.0001, 90)
+    sleep(1)
+    MotorOne.MoveForward(0.0001, 45)
+    sleep(1)
+    MotorOne.MoveBackward(0.0001, 180)
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
+    
+
+    
