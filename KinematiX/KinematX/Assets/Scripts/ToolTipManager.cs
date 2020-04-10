223987539ca1f565ca9242ab2b2e2426b443c103
Nikola <nikola.r.totev@gmail.com>
2020-04-11 00:04:52 +0300

    Adding UI to Software.

---
+++ KinematiX/KinematX/Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ToolTipManager : MonoBehaviour
+{
+
+    private static ToolTipManager m_Instance;
+
+    [SerializeField]
+    private Camera m_UiCamera;
+    public float ToolTipPadding=4.2f;
+    private Text m_ToolTipText;
+    private RectTransform m_BackgroundRectTransform;
+
+    private void Awake()
+    {
+        m_BackgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
+        m_ToolTipText = transform.Find("Txt_Content").GetComponent<Text>();
+
+        ShowToolTip("This is some text so yeah");
+
+        m_Instance = this;
+    }   
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), Input.mousePosition, m_UiCamera, out var localPoint);
+        transform.localPosition = localPoint;
+    }
+
+    private void ShowToolTip(string toolTipText)
+    {
+        gameObject.SetActive(true);
+        m_ToolTipText.text = toolTipText;
+        Vector2 backgroundSize = new Vector2(m_ToolTipText.preferredWidth+ToolTipPadding*2f, m_ToolTipText.preferredHeight+ToolTipPadding*2f);
+        m_BackgroundRectTransform.sizeDelta = backgroundSize;
+
+    }
+
+    private void HideToolTip()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public static void Show(string toolTipString)
+    {
+        m_Instance.ShowToolTip(toolTipString);
+    }
+
+    public static void Hide()
+    {
+        m_Instance.HideToolTip();
+    }
+}
---
+++ KinematiX/KinematX/Assets/Scripts/UI_Navigator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Navigator : MonoBehaviour
+{
+    public enum RobotComponents
+    {
+        Base, RotationBase, ArmSection, RotationJoint, GimbalAssembly
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenProp()
+    {
+
+    }
+    public void OpenPropertyPanel(int enumPosition)
+    {
+        RobotComponents callerType = (RobotComponents) enumPosition;
+        switch (callerType)
+        {
+            case RobotComponents.Base:
+                break;
+            case RobotComponents.RotationBase:
+                break;
+            case RobotComponents.ArmSection:
+                break;
+            case RobotComponents.RotationJoint:
+                break;
+            case RobotComponents.GimbalAssembly:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(callerType), callerType, null);
+        }
+    }
+}
