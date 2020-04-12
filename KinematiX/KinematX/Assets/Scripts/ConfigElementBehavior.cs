f74cd922b1b5aa63e50754c6ae587a9d5029299e
Nikola <nikola.r.totev@gmail.com>
2020-04-12 19:02:01 +0300

    Added ConfigElementBehavior

---
+++ KinematiX/KinematX/Assets/Scripts/ConfigElementBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ConfigElementBehavior : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Action MouseEnterTooltip = null;
+    public Action MouseExitTooltip = null;
+
+    public Action MouseEnter = null;
+    public Action MouseExit = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MouseEnter?.Invoke();
+        MouseEnterTooltip?.Invoke();
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MouseExit?.Invoke();
+        MouseExitTooltip?.Invoke();
+    }
+}
