c31785353acafef6b58f5d8b045cf2071790c925
Nikola <nikola.r.totev@gmail.com>
2020-04-18 14:40:42 -0900

    Updated robot script.

--- Source Code/Motor Scripts/StepperController.py
+++ Source Code/Motor Scripts/StepperController.py
@@ -1,6 +1,6 @@
 from time import sleep
 import RPi.GPIO as GPIO
-class StepppeManager:
+class StepperManager:
     def __init__(self, StepPin, DirPin, StepsPerRev):
         self.StepPin = StepPin
         self.DirPin = DirPin
@@ -9,25 +9,25 @@ class StepppeManager:
             "CW": 1,
             "CCW": 0
         }
-        GPIO.setupMode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.DirPin, GPIO.OUT)
         GPIO.setup(self.StepPin, GPIO.OUT)
 
     def MoveForward(self, speed, angle):
         GPIO.output(self.DirPin, self.DirectionDictionary["CW"])
-        steps = 0.45*angle
+        steps = angle/0.45
 
-        for step in range(steps):
+        for step in range(round(steps)):
             GPIO.output(self.StepPin, GPIO.HIGH)
             sleep(speed)
             GPIO.output(self.StepPin, GPIO.LOW)
             sleep(speed)
 
     def MoveBackward(self, speed,angle):
-        GPIO.output(self.DirPin, self.DirectionDictionary["CW"])
-        steps = 0.45 * angle
+        GPIO.output(self.DirPin, self.DirectionDictionary["CCW"])
+        steps = steps = angle/0.45
 
-        for step in range(steps):
+        for step in range(round(steps)):
             GPIO.output(self.StepPin, GPIO.HIGH)
             sleep(speed)
             GPIO.output(self.StepPin, GPIO.LOW)
--- Source Code/Server Scripts/Robot_Server.py
+++ Source Code/Server Scripts/Robot_Server.py
@@ -1,21 +1,21 @@
 import socket
-
+import time
 
 shouldListen = True
 HEADERLENGTH = 10
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind((socket.gethostname(), 4200))
-serverSocket.listen(1)
-
-
+serverSocket.bind(('192.168.12.119', 4200))
+serverSocket.listen(2)
+print(f"{socket.gethostname()}")
 while shouldListen:
     clientSocket, address = serverSocket.accept()
     print(f"Connection to {address} as been established!")
 
-    msg = "Welcome to the server"
-    msg = f'{len(msg):<{HEADERLENGTH}}' + msg
-
-    clientSocket.send(bytes(msg, "utf-8"))
+    while True:
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = f'{len(msg):<{HEADERLENGTH}}' + msg
+        clientSocket.send(bytes(msg, "utf-8"))
 
 
 
